f4b1d91e536f9bd08b2e51a107dd83601821688d
aml mansour <omartion33@gmail.com>
2023-10-25 22:48:36 +0300

    first commit

---
+++ admin_page.php
@@ -0,0 +1,52 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['admin_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>admin page</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="container" >
+
+   <div class="content">
+   <h1>CMS</h1><h3><span>ADMIN PAGE</span></h3>
+      <h3>welcome <?php echo $_SESSION['admin_name'] ?></h3>     
+   <p></p>
+      <div class="content">
+      <a href="" class="btn">Users Setting</a>
+      <a href="" class="btn">Setting</a>
+      <a href="" class="btn">Discussion Room</a>
+      </div>
+      <div class="content">
+      <img src="img/admin.png" width="300"
+     height="450">
+      </div>
+	    <div class="content">
+       <a href="logout.php" class="btn">logout</a>
+      </div>
+   </div>
+   <p></p>
+
+   
+</div>
+
+</body>
+</html>
---
+++ student_page.php
@@ -0,0 +1,55 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['student_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Student page</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+
+<div class="container" >
+
+   <div class="content">
+   <h1>CMS</h1><h3><span>STUDENT PAGE</span></h3>
+      <h3>welcome <?php echo $_SESSION['student_name'] ?></h3>     
+   <p></p>
+      <div class="content">
+      <a href="" class="btn">Courses Page</a>
+      <a href="" class="btn">Setting</a>
+	  <a href="" class="btn">Discussion Room</a>
+	  <a href="" class="btn">Badges</a>
+	  </div>
+      <div class="content">
+      <img src="img/student.png" width="300"
+     height="450">
+      </div>
+	    <div class="content">
+       <a href="logout.php" class="btn">logout</a>
+      </div>
+   </div>
+   <p></p>
+
+   
+</div>
+
+
+</body>
+</html>
